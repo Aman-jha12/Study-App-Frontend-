d35ce740d2664e63e291bad9917e23bac558b4eb
Aman jha <ajha82703@gmail.com>
2025-08-09 14:06:38 +0530

    Delete backend directory

--- backend/models/Study.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const studySchema = new mongoose.Schema({
-  date: { type: String, required: true, unique: true },
-  totalSeconds: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model("Study", studySchema);
